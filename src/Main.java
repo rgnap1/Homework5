f70f5d027373975a3642caea91b936174eeb8830
rgnap <osovskaya_1993@mail.ru>
2022-11-17 15:31:34 +0300

    Finished task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("//task1");
+
+        //task1
+        int clientOS = 1;
+        if (clientOS==0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else if (clientOS==1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Выберите версию из доступных выше");
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("//task2");
+
+        //task2
+
+
+
+
+
 
 
     }
