f675d3d9f93d1241d723686d344280e5074796e6
rgnap <osovskaya_1993@mail.ru>
2022-11-18 20:16:58 +0300

    Finished task5.

--- src/Main.java
+++ src/Main.java
@@ -65,6 +65,66 @@ public class Main {
         System.out.println("//task4");
 
         //task4
+        int deliveryDistance = 95;
+        int deliveryDistance1Day = 20;
+        int deliveryDistance2Days = 60-20;
+        int deliveryDistance3Days = 100-60;
+
+
+
+
+         //task5
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 1:
+                System.out.println("Зима,январь");
+                break;
+            case 2:
+                System.out.println("Зима,февраль");
+                break;
+            case 3:
+                System.out.println("Весна,март");
+                break;
+            case 4:
+                System.out.println("Весна,апрель");
+                break;
+            case 5:
+                System.out.println("Весна,май");
+                break;
+            case 6:
+                System.out.println("Лето,июнь");
+                break;
+            case 7:
+                System.out.println("Лето,июль");
+                break;
+            case 8:
+                System.out.println("Лето,август");
+                break;
+            case 9:
+                System.out.println("Осень,сентябрь");
+                break;
+            case 10:
+                System.out.println("Осень,октябрь");
+                break;
+            case 11:
+                System.out.println("Осень,ноябрь");
+                break;
+            case 12:
+                System.out.println("Зима,декабрь");
+                break;
+            default:
+                System.out.println("Невозможно определить");
+
+        }
+
+
+
+
+
+
+
+
+
 
 
 
